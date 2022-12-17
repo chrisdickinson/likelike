fa83e73939af459ceb8eb1165eb994dc2bb3265d
Chris Dickinson <chris@neversaw.us>
2022-12-16 21:51:17 -0800

    "Oh, I'll write a plist!" I said, excitedly.
    Little did I know what I had signed up for.
    
    Anyway, watchexec now has permission to read photos of me from 2007.
    Just in case they change. God, wouldn't that be unnerving?
    
    As always, this foray quickly turned into "let's write some quick and dirty
    bash." In the spirit of the last commit: before I forget, there's one problem
    here. That is: whenever I edit _any_ notes, it's very likely that the SQLite
    database file will change, despite no real updates having occurred. This'll
    trigger a re-render of my blog. I'll have to keep an eye on this.

---
+++ bin/install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if ! &>/dev/null command -v watchexec; then
+  brew install watchexec
+fi
+
+if ! &>/dev/null command -v likelike; then
+  gh release download --repo chrisdickinson/likelike -p '*x64_macos*'
+  tar zxfv likelike*.tar.gz
+  rm likelike*.tar.gz
+  mv likelike ~/bin/
+fi
+
+log_file=~/.local/state/likelike/log
+watchexec_path=$(command -v watchexec)
+likelike_path=$(command -v likelike)
+notes_dir=~/notes/
+
+if [ ! -e "$(dirname "$log_file")" ]; then
+  mkdir -p "$(dirname "$log_file")"
+fi
+
+cat > ~/Library/LaunchAgents/us.neversaw.likelike.plist <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>KeepAlive</key>
+  <true/>
+
+	<key>Label</key>
+  <string>us.neversaw.likelike</string>
+
+	<key>ProgramArguments</key>
+  <array>
+    <string>${watchexec_path}</string>
+    <string>-e</string>
+    <string>md</string>
+    <string>-w</string>
+    <string>${notes_dir}</string>
+    <string>-p</string>
+    <string>--</string>
+    <string><![CDATA[${likelike_path} import ${notes_dir} && cp "$HOME/Library/Application Support/likelike.sqlite3" "$HOME/blog/"]]></string>
+  </array>
+
+	<key>RunAtLoad</key>
+  <true/>
+
+	<key>StandardErrorPath</key>
+  <string>${log_file}</string>
+
+	<key>StandardOutPath</key>
+  <string>${log_file}</string>
+
+	<key>WorkingDirectory</key>
+  <string>${notes_dir}</string>
+</dict>
+</plist>
+EOF
+launchctl load ~/Library/LaunchAgents/us.neversaw.likelike.plist
